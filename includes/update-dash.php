c8d0687803859e32df0b4faa48fba7b79b153f36
FRS077 <64902214+FRS077@users.noreply.github.com>
2025-10-23 23:36:54 +0200

    FRS077

--- includes/update-dash.php
+++ includes/update-dash.php
@@ -1,17 +1,9 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm_update'])) {
-    if ($_POST['confirm_update'] === 'yes') {
-        exec('sudo /bin/mount -o remount,rw / && sudo /usr/local/bin/update-dash.sh && sudo /bin/mount -o remount,ro /');
-        echo "Mise à jour effectuée avec succès.";
-    } else {
-        echo "Mise à jour annulée.";
-    }
+exec('sudo /bin/mount -o remount,rw / && sudo /usr/local/bin/update-dash.sh && sudo /bin/mount -o remount,ro /', $output, $return_var);
+
+if ($return_var === 0) {
+    echo "Mise à jour effectuée avec succès.";
 } else {
-    // Affiche le formulaire de confirmation
-    echo '<form method="post">
-            <p>Voulez-vous vraiment lancer la mise à jour du Dashboard ?</p>
-            <button type="submit" name="confirm_update" value="yes">Oui</button>
-            <button type="submit" name="confirm_update" value="no">Non</button>
-          </form>';
+    echo "Erreur lors de la mise à jour :<br>" . implode("<br>", $output);
 }
 ?>
