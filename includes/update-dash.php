15f0087f760cd7bcdddb974df84623bdeb65d6d8
FRS077 <64902214+FRS077@users.noreply.github.com>
2025-10-23 23:38:58 +0200

    FRS077

--- includes/update-dash.php
+++ includes/update-dash.php
@@ -2,8 +2,11 @@
 exec('sudo /bin/mount -o remount,rw / && sudo /usr/local/bin/update-dash.sh && sudo /bin/mount -o remount,ro /', $output, $return_var);
 
 if ($return_var === 0) {
-    echo "Mise à jour effectuée avec succès.";
+    // Redirection vers index.php après succès
+    header('Location: /index.php');
+    exit;
 } else {
+    // En cas d'erreur, affichage du message
     echo "Erreur lors de la mise à jour :<br>" . implode("<br>", $output);
 }
 ?>
