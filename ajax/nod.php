2251a2c7e53094112ef15820be67b724a060c092
FRS077 <64902214+FRS077@users.noreply.github.com>
2025-11-28 23:49:24 +0100

    frs077

--- ajax/nod.php
+++ ajax/nod.php
@@ -130,7 +130,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <style>
     body {
-        background-color: #f8f9fa;
+        background-color: #0472e0;
         padding: 30px;
         font-family: Arial, sans-serif;
         color: #444;
