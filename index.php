64a112a27ad5fd3371b39082c64ebc118be12852
FRS077 <64902214+FRS077@users.noreply.github.com>
2026-08-19 17:53:37 +0200

    FRS077

--- index.php
+++ index.php
@@ -185,7 +185,7 @@ switch ($page) {
                     <a class="<?php echo ($page == 'tty') ? 'active' : ''; ?> list-group-item list-group-item-action list-group-item-light p-3" href="./?p=tty">💻 Terminal</a>
                     <a class="<?php echo ($page == 'cfg') ? 'active' : ''; ?> list-group-item list-group-item-action list-group-item-light p-3" href="./?p=cfg">⚙️ Config</a>
                     <a class="<?php echo ($page == 'nod') ? 'active' : ''; ?> list-group-item list-group-item-action list-group-item-light p-3" href="./?p=nod">ℹ️ Node Info</a>
-                    <a class="<?php echo ($page == 'ext') ? 'active' : ''; ?> list-group-item list-group-item-action list-group-item-light p-3" href="http://www.f62dmr.fr/svxrdb/index.php" target="_blank">🌐 Dashboard RNFA</a>
+                    <a class="<?php echo ($page == 'ext') ? 'active' : ''; ?> list-group-item list-group-item-action list-group-item-light p-3" href="https://www.f62dmr.fr/index.php" target="_blank">🌐 Dashboard RNFA</a>
                     <a class="list-group-item list-group-item-action list-group-item-light p-3" href="https://www.facebook.com/groups/1067389751809869" target="_blank">📘 Groupe Facebook</a>
                     <a class="<?php echo ($page == 'aid') ? 'active' : ''; ?>list-group-item list-group-item-action list-group-item-light p-3"href="/includes/aide.php" target="_blank">❓ Aide</a>
             </div>
@@ -208,7 +208,7 @@ switch ($page) {
 								<li class="nav-item"><a class="<?php echo ($page == 'tty') ? 'active p-2' : ''; ?> nav-link" href="./?p=tty">💻 Terminal</a></li>
                                 <li class="nav-item"><a class="<?php echo ($page == 'cfg') ? 'active p-2' : ''; ?> nav-link" href="./?p=cfg">⚙️ Config</a></li>
                                 <li class="nav-item"><a class="<?php echo ($page == 'nod') ? 'active p-2' : ''; ?> nav-link" href="./?p=nod">ℹ️ Node Info</a></li>
-                                <li class="nav-item"><a class="nav-link p-2" href="http://www.f62dmr.fr/svxrdb/index.php" target="_blank">🌐 Dashboard du RNFA</a></li>
+                                <li class="nav-item"><a class="nav-link p-2" href="https://www.f62dmr.fr/index.php" target="_blank">🌐 Dashboard du RNFA</a></li>
                                 <li class="nav-item"><a class="nav-link p-2" href="https://www.facebook.com/groups/1067389751809869" target="_blank">📘 Groupe Facebook</a></li>
                                 <li class="nav-item"><a class="<?php echo ($page == 'aid') ? 'active p-2' : ''; ?> nav-link" href="/includes/aide.php" target="_blank">❓ Aide</a></li>
                             </ul>
